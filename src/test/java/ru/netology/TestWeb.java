4e504c88ceccbb60c54b0b016a488825efde56af
Dmitry <dmitry.ryazantsev@mail.ru>
2023-07-09 12:20:23 +0300

    firstcommit

--- src/test/java/ru/netology/TestWeb.java
+++ src/test/java/ru/netology/TestWeb.java
@@ -39,12 +39,11 @@ public class TestWeb {
     }
     @Test
     void shouldTest() {
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий Васильев-Прутов");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79280000000");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
